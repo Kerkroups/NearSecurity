ccd28234c45627a30ee03c8305569451aea73882
Hlib Yavorskyi <Kerkroups@gmail.com>
2026-09-02 17:36:28 +0300

    Update PotyzhniyValidator.php

--- PHP/PotyzhniyValidator.php
+++ PHP/PotyzhniyValidator.php
@@ -4,12 +4,38 @@ class PotyzhniyValidator {
 
 	public function __construct() {
 
+		private $data1;
+		private $data2;
+		$data1_type = gettype($data1);
+		$data2_type = gettype($data2);
+
 		// Реализовать конструктор;
 	}
 	
 	// Конвертация строки в двоичный формат и создание массива для строки.
 	// Возможно стоит реализовать способ с преобразованием массива в последовательность 0 и 1.
-	function stringToBinaryString(string $str) {
+	protected function objectToBinaryString(object $int) {
+    	$binary = [];
+
+    	// ...
+    	return $binary;
+	}
+	
+	protected function floatToBinaryString(float $int) {
+    	$binary = [];
+
+    	// ...
+    	return $binary;
+	}
+	
+	protected function integerToBinaryString(integer $int) {
+    	$binary = [];
+
+    	// ...
+    	return $binary;
+	}
+	
+	protected function stringToBinaryString(string $str) {
     	$binary = [];
 
     	foreach (str_split($str) as $char) {
@@ -17,10 +43,34 @@ class PotyzhniyValidator {
     	}
     	return $binary;
 	}
-
+	/* Возможно нужно сделать общую функцию с case:
+	public static function checkData($data1, $data2){
+		if ($data1_type === $data1_type) {
+			$type = gettype($data1_type);
+			switch($type) {
+				case 'object':
+					compareObjects();
+					break;
+				case 'float':
+					compareFloats();
+					break;
+				case 'integer':
+					compareItegers();
+					break;
+				case 'string':
+					compareStrings()
+					break;
+			}
+		}
+		else {
+			echo "Unknown data type";
+		}
+	}
+		
+	*/
 	// Статический метод для сравнениия массивов.
 	// Возможно стоит добавить возможность сравнивать последовательности из 1 для двух строк а именно их индексы.
-	public static function checkArrays(array $arr1, array $arr2){
+	public static function compareStrings(array $arr1, array $arr2){
 		$a1 = stringToBinaryString($arr1);
 		$a2 = stringToBinaryString($arr2);
 		if (count($a1) === count($a2) && $a1 === $a2) {
@@ -31,7 +81,6 @@ class PotyzhniyValidator {
 			echo "Strings not same";
 			return False;
 		}
-
 	}
 
 // $str1 = stringToBinaryString($_GET['a']);
@@ -43,7 +92,7 @@ $array1 = explode(",", $string);
 $string2 = "string";
 $array2 = explode(",", $string2);
 
-$result = checkArrays($array1, $array2);
+$result = compareStrings($array1, $array2);
 
 //echo '<pre>';
 //print_r($str1);
